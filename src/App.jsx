d82bdca7eeba709d1c2c261ef2805e0454ea0d29
Hasan Majid <hasanmajid3.14@hotmail.com>
2024-03-10 01:14:26 -0500

    Refactor code and update UI components

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Heading } from "@chakra-ui/react"
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      <h1>hello world</h1>
-      <h2>hello mars</h2>
+      <Heading>
+        Hello World
+      </Heading>
     </div>
   )
 }
