b48e3e72feff2552b96aa66c61b4a8d2b57e5f5b
Hasan Majid <hasanmajid3.14@hotmail.com>
2024-03-10 15:40:09 -0400

    Add CORS package and enable CORS for all origins

--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const express = require("express");
+const cors = require('cors');
 const app = express();
-const port = 8000;
+const port = process.env.PORT || 8000;
+
+// Middleware to parse JSON requests
+app.use(express.json());
+// Enable CORS for all origins
+app.use(cors());
+
+// const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost:27017/blogDB', { useNewUrlParser: true, useUnifiedTopology: true });
+// const postSchema = new mongoose.Schema({ title: String, content: String });
+// const Post = mongoose.model('Post', postSchema);
 
 app.get("/", function (req, res) {
     res.send("Hello World!");
