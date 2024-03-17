ad401b288047a7d4ab49a7daf29e8651ed7dec7b
Hasan Majid <hasanmajid3.14@hotmail.com>
2024-03-16 23:08:12 -0400

    Add routing and navigation components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,31 @@
-import { Heading } from "@chakra-ui/react"
+import { BrowserRouter, Route, Routes, Link } from 'react-router-dom';
+import ItemsWanted from "./Pages/ItemsWanted";
+import Home from "./Pages/Home";
+import ItemsForSale from "./Pages/ItemsForSale";
+import Services from "./Pages/Services";
+import Navbar from "./components/navbar/Navbar";
+import Compose from "./components/Compose";
 
 function App() {
   return (
-    <div>
-      <Heading>
-        Hello World
-      </Heading>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Link to="/" style={{ marginInline: "1rem" }}>Home </Link>
+
+      <Link to="/items-wanted" style={{ marginInline: "1rem" }}>Items Wanted</Link>
+
+      <Link to="/items-for-sale" style={{ marginInline: "2rem" }}>Items for Sale</Link>
+
+      <Link to="/services" style={{ marginInline: "1rem" }}>Services</Link>
+
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route path="/items-wanted" element={<ItemsWanted />} />
+        <Route path="/items-for-sale" element={<ItemsForSale />} />
+        <Route path="/services" element={<Services />} />
+      </Routes>
+      <Compose />
+    </BrowserRouter>
   )
 }
 
