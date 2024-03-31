11170141542eb8552800a6976fbadb29b1354fd3
Hasan Majid <hasanmajid3.14@hotmail.com>
2024-03-31 14:09:36 -0400

    Add user routes and connect to MongoDB

---
+++ server/routes/User.route.js
@@ -0,0 +1,46 @@
+const express = require("express")
+const router = express.Router()
+const mongoose = require('mongoose'); //Import mongoDB
+
+
+const userSchema = new mongoose.Schema({ email: String, firstName: String, lastName: String, password: String });
+const User = mongoose.model('User', userSchema);
+
+router.post("/signup", async (req, res) => {
+    console.log("signing up")
+    const {email, firstName, lastName, password} = req.body;
+    
+    // Todo: check email does not already exist in db
+
+    if (!email || !firstName  || !lastName || !password) {
+        return res.status(400).json({ error: 'Title and content are required' });
+    }
+    
+    try {
+        const newUser = await User.create({email: "bob@gmail.com", firstName: "bob", lastName: "jones", password: "1234"})
+        return res.status(201).json(newUser);
+    } catch {
+        return res.sendStatus(400);
+    }
+})
+
+router.get("/login", async (req, res) => {
+    res.send("Logged in")
+    const {email, password} = req.query;
+    console.log(email, password)
+    
+    if (!email || !password) {
+        return res.status(400).json({ error: 'Title and content are required' });
+    }
+
+    try{
+        const user = await User.find({email: email, password: password})
+        console.log(user)
+        return res.json(user)
+    } catch{
+        return res.status(400);
+    }
+
+})
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -3,15 +3,24 @@ const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 8000;
 
+const userRoute = require("./routes/User.route");
+
 // Middleware to parse JSON requests
 app.use(express.json());
-// Enable CORS for all origins
-app.use(cors());
 
-// const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost:27017/blogDB', { useNewUrlParser: true, useUnifiedTopology: true });
-// const postSchema = new mongoose.Schema({ title: String, content: String });
-// const Post = mongoose.model('Post', postSchema);
+// Add CORS headers (Only allows our domain to access our server)
+app.use(cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    exposedHeaders: ["Authorization", "newToken"]
+}))
+
+app.use("/user", userRoute);
+
+
+//Connect to MongoDB
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/tmu-ads', { useNewUrlParser: true, useUnifiedTopology: true });
 
 app.get("/", function (req, res) {
     res.send("Hello World!");
