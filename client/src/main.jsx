3600b7b06b2d54cdf58f30d793f1f69accdf0aaa
Hasan Majid <hasanmajid3.14@hotmail.com>
2024-04-01 15:20:15 -0400

    Add QueryClientProvider to main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,6 +2,12 @@ import { ChakraProvider, extendTheme } from '@chakra-ui/react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import UserContextProvider from './context/UserContext.jsx'
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
+
+const queryClient = new QueryClient()
 
 // 1. Using a style object
 const theme = extendTheme({
@@ -18,9 +24,11 @@ const theme = extendTheme({
 })
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <ChakraProvider theme={theme}>
-    <UserContextProvider>
-      <App />
-    </UserContextProvider>
-  </ChakraProvider>
+  <QueryClientProvider client={queryClient}>
+    <ChakraProvider theme={theme}>
+      <UserContextProvider>
+        <App />
+      </UserContextProvider>
+    </ChakraProvider>
+  </QueryClientProvider>
 )
