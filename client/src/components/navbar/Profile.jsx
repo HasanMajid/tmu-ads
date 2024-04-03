cf05340f34a1ee8b26646fe39b7b9ecaaad67ecb
Hasan Majid <hasanmajid3.14@hotmail.com>
2024-04-03 15:57:24 -0400

    User profile navigates to settings page

--- client/src/components/navbar/Profile.jsx
+++ client/src/components/navbar/Profile.jsx
@@ -1,11 +1,21 @@
 import { Button, Box } from "@chakra-ui/react";
-
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
+import { useContext } from "react";
 
 function Profile({children}) {
+    const {user} = useContext(UserContext);
+    const navigate = useNavigate();
+
+    //Check database for email and password
+    const handleSubmit = () => {
+      navigate("/settings")
+    };
+
     return (
         <Box >
           <Button colorScheme='teal' variant='outline' 
-          style={{borderRadius: "50%", height: "4rem", width: "4rem"}}>{children}</Button>
+          style={{borderRadius: "50%", height: "4rem", width: "4rem"}} onClick={handleSubmit}>{children}</Button>
         </Box>
     );
 }
