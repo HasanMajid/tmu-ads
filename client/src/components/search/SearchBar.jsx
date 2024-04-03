6f862cbaaa089bfdb0374fc2d05fa0deddc5b701
Hasan Majid <hasanmajid3.14@hotmail.com>
2024-04-02 22:45:42 -0400

    Add SearchBar component

---
+++ client/src/components/search/SearchBar.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import { Flex, Input, Select, Button } from '@chakra-ui/react';
+
+function SearchBar() {
+  const [option, setOption] = useState("All");
+
+  return (
+    <Flex maxW={"50rem"} padding={"2rem"} m={"auto"}>
+      <Input variant="filled" placeholder="Search Ads" />
+      <Select onChange={(e) => {
+        setOption(e.target.value);
+      }}>
+        <option>All</option>
+        <option>Items wanted</option>
+        <option>Item for Sale</option>
+        <option>Services</option>
+      </Select>
+      <Button p={"1rem"}>Search</Button>
+    </Flex>
+  )
+}
+
+export default SearchBar
