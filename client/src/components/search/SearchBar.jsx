ecaff5f9471eda5334e71c0b46f6c33fbb025855
Hasan Majid <hasanmajid3.14@hotmail.com>
2024-04-02 23:42:51 -0400

    Refactor search bar component

--- client/src/components/search/SearchBar.jsx
+++ client/src/components/search/SearchBar.jsx
@@ -1,25 +1,56 @@
-import { useEffect, useState } from 'react';
-import { Flex, Input, Select, Button } from '@chakra-ui/react';
+import { useEffect, useState } from "react";
+import { Flex, Input, Select, Button } from "@chakra-ui/react";
+import axios from "axios";
+import { url } from "../../utils/constants";
+import { useNavigate } from "react-router-dom";
 
 function SearchBar() {
-  const [option, setOption] = useState("All");
+  const navigate = useNavigate();
+  const [type, setType] = useState("All");
+  const [search, setSearch] = useState(null);
 
   return (
     <Flex maxW={"50rem"} padding={"2rem"} m={"auto"}>
-      <Input variant="filled" placeholder="Search Ads" />
-      <Select onChange={(e) => {
-        setOption(e.target.value);
-      }}>
+      <Input
+        variant="filled"
+        placeholder="Search Ads"
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      />
+
+      <Select
+        onChange={(e) => {
+          setType(e.target.value);
+        }}
+      >
         <option>All</option>
-        <option>Items wanted</option>
+        <option>Item Wanted</option>
         <option>Item for Sale</option>
-        <option>Services</option>
+        <option>Service</option>
       </Select>
+
       <Flex>
-        <Button p={"1rem"}>Search</Button>
+        <Button
+          p={"1rem"}
+          onClick={() => {
+            if (search) {
+            console.log("searching")
+              navigate("/searchresults", {
+                state: {
+                  search: search,
+                  type: type,
+                },
+              });
+            }
+          }}
+        >
+          Search
+        </Button>
       </Flex>
+      
     </Flex>
-  )
+  );
 }
 
-export default SearchBar
+export default SearchBar;
