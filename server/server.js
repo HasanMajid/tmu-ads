2eadf39a710ef487b95ec95999d10b429462f233
Hasan Majid <hasanmajid3.14@hotmail.com>
2024-04-03 21:02:26 -0400

    Add socket.io functionality and message route

--- server/server.js
+++ server/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const cors = require('cors');
-const app = express();
+const http = require('http');
+const socketIo = require('socket.io');
 const port = process.env.PORT || 8000;
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+require('dotenv').config();
 
 const userRoute = require("./routes/User.route");
 const adPostRoute = require("./routes/AdPost.route");
+const messageRoute = require("./routes/Message.route");
 
 // Middleware to parse JSON requests
 app.use(express.json());
@@ -18,7 +24,13 @@ app.use(cors({
 
 app.use("/user", userRoute);
 app.use("/adpost", adPostRoute);
+app.use("/message", messageRoute);
 
+// Middleware
+app.use((req, res, next) => {
+    res.io = io;
+    next();
+});
 
 //Connect to MongoDB
 const mongoose = require('mongoose');
